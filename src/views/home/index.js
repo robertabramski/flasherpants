5b2f2bf488cd338198cea2a96c8b846c5c8eec02
Robert Abramski <iam@robertabramski.com>
2019-01-19 09:33:31 -1000

    Remove NavHeader for now.

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import css from './style.scss';
-import NavHeader from 'components/nav-header';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div className={css.module}>
-        <NavHeader />
         <h1>Welcome to Flasherpants</h1>
       </div>
     );
